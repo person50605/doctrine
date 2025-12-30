2030cf611ed1bdaebdd5a6ecb2884102514e6ffb
person50605 <124418091+person50605@users.noreply.github.com>
2025-12-29 16:48:27 -0800

    Fix typo in fishing action description

--- src/views/building/ItemActions.vue
+++ src/views/building/ItemActions.vue
@@ -9,7 +9,7 @@
 
         <div class="subtitle-1">Example: fishing</div>
 
-        <p>Imagine you want to add a 'fishing' action to a number of lakes and shorelines on your world. You could define a room action in each location where a player might want to fish, but would need to create as many actions as there fishign locations. Instead, you can create one item, named for example "A school of fish swims beneath the surface." with a 'fish' action tied to it, make the item non-pickable and load it everywhere you want players to be able to fish.</p>
+        <p>Imagine you want to add a 'fishing' action to a number of lakes and shorelines on your world. You could define a room action in each location where a player might want to fish, but would need to create as many actions as there fishing locations. Instead, you can create one item, named for example "A school of fish swims beneath the surface." with a 'fish' action tied to it, make the item non-pickable and load it everywhere you want players to be able to fish.</p>
 
       </template>
     </Article>
